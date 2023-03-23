9e43d438e78a164688d810a9494cb018e4abff8c
YoqubjonovSaidumarxon <saidumarxonyoqubjonov3133@gmail.com>
2023-03-23 15:16:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// // // // // // // // // // // let yosh = 14;
+// // // // // // // // // // // alert(`Yoshim ${yosh}da.`)
+// // // // // // // // // // function x2(a){ var b = a * 2; return b;}
+// // // // // // // // // // // var c = x2(12);
+// // // // // // // // // // // alert(c);
+// // // // // // // // // // function x2(a){
+// // // // // // // // // // 	i = a * a;
+// // // // // // // // // // 	return i;
+// // // // // // // // // // }
+// // // // // // var i = 10;
+// // // // // // var c = x2(12);
+// // // // // // alert(i);
+// // // // // // // // // (
+
+// // // // // // // // // function dskr(a, b, c) {
+// // // // // // // // // 	var d = b * b - 4 * a * c;
+// // // // // // // // // 	return d; 
+// // // // // // // // // }
+
+// // // // // // // // // 	);
+// // // // // // // // // var d = dskr(1, 2, 0);
+// // // // // // // // function f() {return true; }
+// // // // // // // // f = function () {return true;}
+// // // // // // // alert(f);
+// // // // // // fun = function f() {
+// // // // // //     f = 'Ali Bobo';
+// // // // // //     alert(f);
+// // // // // // }
+// // // // // var yosh = 40;
+// // // // // alert(`Yoshim ${yosh}da.`)
+// // // // // console.log(yosh);
+// // // // for (let i = 1; i < 4; i++) {
+// // // //     console.log(i + '-qadam');
+    
+// // // // // }
+// // // // for (let i = 1; ; i++) {
+// // // //     console.log(i + '-qadam');
+// // // //     if (i > 10) break;
+// // // //  }
+// // //  var inson = {
+// // //    ism: "Anvar",
+// // //    yosh: 39,
+// // //    erkak: true
+// // //  };
+// // //  console.log(inson);
+// // //  console.log(inson.ism);
+// //     var elem = 'ism';
+// //     console.log(inson[elem]);
+// const obj = {jinsi: "Erkak"};
+// obj = 39;
+// obj['jinsi'] = "Ayol";
+var yosh = 15;
+console.log(yosh);
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../w3school/Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="index.css">
+	<title>Document</title>
+</head>
+<body>
+	<div class="sidebar">
+  <h2>My Sidebar</h2>
+  <ul>
+    <li><a href="#">Link 1</a></li>
+    <li><a href="#">Link 2</a></li>
+    <li><a href="#">Link 3</a></li>
+  </ul>
+</div>
+
+</body>
+</html>
